e9b8752d0c96b9240fec4d71f1fc1805fccb6752
KyungSeop.Chung <xseleven@gmail.com>
2021-04-20 09:22:49 +0900

    Added main.c

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+	int nData = 0;
+
+	fprintf(stdout, "No = %d\n", nData);
+
+	exit(EXIT_SUCCESS);
+}
